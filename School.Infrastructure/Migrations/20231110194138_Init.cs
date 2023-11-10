c6fecb1f56c5965649eda77d4bd229e26eaa13c6
JakubRoss <Jakub.Rosploch@gmail.com>
2023-11-10 20:45:06 +0100

    ef installation and first migration

---
+++ School.Infrastructure/Migrations/20231110194138_Init.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SchoolClass",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClassName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchoolClass", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teacher",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Post = table.Column<float>(type: "real", nullable: true),
+                    StartOfWork = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teacher", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GradeAvarage = table.Column<float>(type: "real", nullable: true),
+                    SchoolClassId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Student_SchoolClass_SchoolClassId",
+                        column: x => x.SchoolClassId,
+                        principalTable: "SchoolClass",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SchoolClassTeacher",
+                columns: table => new
+                {
+                    SchoolClassesId = table.Column<int>(type: "int", nullable: false),
+                    TeachersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchoolClassTeacher", x => new { x.SchoolClassesId, x.TeachersId });
+                    table.ForeignKey(
+                        name: "FK_SchoolClassTeacher_SchoolClass_SchoolClassesId",
+                        column: x => x.SchoolClassesId,
+                        principalTable: "SchoolClass",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SchoolClassTeacher_Teacher_TeachersId",
+                        column: x => x.TeachersId,
+                        principalTable: "Teacher",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SchoolClassTeacher_TeachersId",
+                table: "SchoolClassTeacher",
+                column: "TeachersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_SchoolClassId",
+                table: "Student",
+                column: "SchoolClassId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SchoolClassTeacher");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Teacher");
+
+            migrationBuilder.DropTable(
+                name: "SchoolClass");
+        }
+    }
+}
---
+++ School.Infrastructure/Persistance/SchoolDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.WebAPI.Domain.Entities;
+
+namespace School.Infrastructure.Persistance
+{
+    public class SchoolDbContext : DbContext
+    {
+        public DbSet<SchoolClass> SchoolClass { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Teacher> Teacher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=OMEN;Database=School.EF.KZL;Trusted_Connection=True;Integrated Security=true;TrustServerCertificate=true;");
+        }
+    }
+}
